fca32455391ea7d743f5828b9b64f8da3c995ab9
Toby King <tobyking13@gmail.com>
2023-01-22 18:40:01 +0000

    simplifies code and makes it less repetative

--- starter/script.js
+++ starter/script.js
@@ -1,16 +1,20 @@
-getSchedule();
-
 var day = {
-  hour9: JSON.parse(localStorage.getItem("hour9")),
-  hour10: JSON.parse(localStorage.getItem("hour10")),
-  hour11: JSON.parse(localStorage.getItem("hour11")),
-  hour12: JSON.parse(localStorage.getItem("hour12")),
-  hour13: JSON.parse(localStorage.getItem("hour13")),
-  hour14: JSON.parse(localStorage.getItem("hour14")),
-  hour15: JSON.parse(localStorage.getItem("hour15")),
-  hour16: JSON.parse(localStorage.getItem("hour16")),
-  hour17: JSON.parse(localStorage.getItem("hour17")),
+  hour9: '',
+  hour10: '',
+  hour11: '',
+  hour12: '',
+  hour13: '',
+  hour14: '',
+  hour15: '',
+  hour16: '',
+  hour17: ''
 };
+
+function localStorageToObj() {
+  for (var i = 9; i <= 17; i++) {
+    day['hour' + i] = JSON.parse(localStorage.getItem('hour' + i));
+  }
+}
 
 function getTime() {
   var time = $("#currentDay");
@@ -45,85 +49,111 @@ function timeColorCode() {
 }
 
 function getSchedule() {
-  $("document").ready(function () {
-    $("#text9").val(day.hour9);
-  });
-  $("document").ready(function () {
-    $("#text10").val(day.hour10);
-  });
-  $("document").ready(function () {
-    $("#text11").val(day.hour11);
-  });
-  $("document").ready(function () {
-    $("#text12").val(day.hour12);
-  });
-  $("document").ready(function () {
-    $("#text13").val(day.hour13);
-  });
-  $("document").ready(function () {
-    $("#text14").val(day.hour14);
-  });
-  $("document").ready(function () {
-    $("#text15").val(day.hour15);
-  });
-  $("document").ready(function () {
-    $("#text16").val(day.hour16);
-  });
-  $("document").ready(function () {
-    $("#text17").val(day.hour17);
-  });
+  for (var i = 9; i <= 17; i++) {
+      var text = $('#text' + (i))
+      var hour = day['hour' + i]
+      text.val(hour)
+  }
 }
 
-
-
 function setSchedule() {
-  $("document").ready(function () {
-    $("#btn9").click(function (e) {
-      let hour9Text = $("#text9").val().trim();
-      localStorage.setItem("hour9", JSON.stringify(hour9Text));
-    });
-    $("#btn10").click(function () {
-      let hour10Text = $("#text10").val().trim();
-      localStorage.setItem("hour10", JSON.stringify(hour10Text));
-    });
-    $("#btn11").click(function () {
-      let hour11Text = $("#text11").val().trim();
-      localStorage.setItem("hour11", JSON.stringify(hour11Text));
-    });
-    $("#btn12").click(function () {
-      let hour12Text = $("#text12").val().trim();
-      localStorage.setItem("hour12", JSON.stringify(hour12Text));
-    });
-    $("#btn13").click(function () {
-      let hour13Text = $("#text13").val().trim();
-      localStorage.setItem("hour13", JSON.stringify(hour13Text));
-    });
-    $("#btn14").click(function () {
-      let hour14Text = $("#text14").val().trim();
-      localStorage.setItem("hour14", JSON.stringify(hour14Text));
-    });
-    $("#btn15").click(function () {
-      let hour15Text = $("#text15").val().trim();
-      localStorage.setItem("hour15", JSON.stringify(hour15Text));
-    });
-    $("#btn16").click(function () {
-      let hour16Text = $("#text16").val().trim();
-      localStorage.setItem("hour16", JSON.stringify(hour16Text));
-    });
-    $("#btn17").click(function () {
-      let hour17Text = $("#text17").val().trim();
-      localStorage.setItem("hour17", JSON.stringify(hour17Text));
-    });
-  });
+  for (let i = 9; i <= 17; i++) {
+    $('#btn' + i).click(function(e) {
+      var textArea = $('#text' + i);
+      var textareaValue = JSON.parse(localStorage.getItem("hour" + i));
+      textareaValue = textArea.val();
+      day["hour" + i] = textareaValue;
+      localStorage.setItem("hour" + i, JSON.stringify(textareaValue));
+  })
+  }
 }
 
 getTime();
 timeColorCode();
-setSchedule();
 
+$(document).ready(function(){
+  localStorageToObj();
+  setSchedule();
+  getSchedule();
+});
 
 // Too much repetative code. Look for ways to simplify things
-// style the page so it looks good.
 
 // add a button to clear the hour
 // when user hits enter textarea saves
+
+// dropdown menu to change timezone
+
+
+
+
+// function getSchedule() {
+//   $("document").ready(function () {
+//     $("#text9").val(day.hour9);
+//   });
+//   $("document").ready(function () {
+//     $("#text10").val(day.hour10);
+//   });
+//   $("document").ready(function () {
+//     $("#text11").val(day.hour11);
+//   });
+//   $("document").ready(function () {
+//     $("#text12").val(day.hour12);
+//   });
+//   $("document").ready(function () {
+//     $("#text13").val(day.hour13);
+//   });
+//   $("document").ready(function () {
+//     $("#text14").val(day.hour14);
+//   });
+//   $("document").ready(function () {
+//     $("#text15").val(day.hour15);
+//   });
+//   $("document").ready(function () {
+//     $("#text16").val(day.hour16);
+//   });
+//   $("document").ready(function () {
+//     $("#text17").val(day.hour17);
+//   });
+// }
+
+// function setSchedule() {
+//   $("document").ready(function () {
+//     $("#btn9").click(function (e) {
+//       let hour9Text = $("#text9").val().trim();
+//       localStorage.setItem("hour9", JSON.stringify(hour9Text));
+//     });
+//     $("#btn10").click(function () {
+//       let hour10Text = $("#text10").val().trim();
+//       localStorage.setItem("hour10", JSON.stringify(hour10Text));
+//     });
+//     $("#btn11").click(function () {
+//       let hour11Text = $("#text11").val().trim();
+//       localStorage.setItem("hour11", JSON.stringify(hour11Text));
+//     });
+//     $("#btn12").click(function () {
+//       let hour12Text = $("#text12").val().trim();
+//       localStorage.setItem("hour12", JSON.stringify(hour12Text));
+//     });
+//     $("#btn13").click(function () {
+//       let hour13Text = $("#text13").val().trim();
+//       localStorage.setItem("hour13", JSON.stringify(hour13Text));
+//     });
+//     $("#btn14").click(function () {
+//       let hour14Text = $("#text14").val().trim();
+//       localStorage.setItem("hour14", JSON.stringify(hour14Text));
+//     });
+//     $("#btn15").click(function () {
+//       let hour15Text = $("#text15").val().trim();
+//       localStorage.setItem("hour15", JSON.stringify(hour15Text));
+//     });
+//     $("#btn16").click(function () {
+//       let hour16Text = $("#text16").val().trim();
+//       localStorage.setItem("hour16", JSON.stringify(hour16Text));
+//     });
+//     $("#btn17").click(function () {
+//       let hour17Text = $("#text17").val().trim();
+//       localStorage.setItem("hour17", JSON.stringify(hour17Text));
+//     });
+//   });
+// }
